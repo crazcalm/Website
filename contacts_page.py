1c3dc8ecf0ad641b217d4d664a4f8f1f431cc338
Marcus Willock <crazcalm@gmail.com>
2013-07-11 01:51:35 -0400

    Create contacts_page.py

---
+++ contacts_page.py
@@ -0,0 +1,31 @@
+/*
+
+Looking back on these files, they seem so much simplier now...
+
+
+*/
+
+
+
+img{
+  
+	height: 150px;
+	width : 150px;
+	
+}
+
+#contacts{
+	
+	height:inherit;
+	width:inherit;
+	margin-top: 0%;
+	margin-left:-25%;
+	
+	
+	border: none;
+}
+
+#email{
+	margin-left: -18%;
+	text-align: center;
+}
