8003ebed30600c9115b0bb6131216c987fdac271
Marcus Willock <crazcalm@gmail.com>
2013-07-11 01:53:37 -0400

    Create portfolio_page.py

---
+++ portfolio_page.py
@@ -0,0 +1,87 @@
+/*
+
+This is more like it!
+
+*/
+
+
+
+.content{
+  
+	
+	
+	margin-left:5%;
+	margin-top: 1%;
+	width: 75%;
+}
+
+.side_bar{
+	
+	height: 100%;
+	background-color: red;
+}
+
+
+.portfolio_item{
+	
+	width: inherit;
+	height: 150px;
+	
+	text-align: center;
+	
+	float: left;
+	
+	border: 2px;
+	border-color: #cccccc;
+	border-style:double;
+	
+	margin-top: 34px;
+}
+
+.item_title{
+	
+	background-color: #33ffff;
+	width: 83%;
+	height: 20%;
+	
+	float: left;
+	
+	padding-bottom: 20px;
+	margin-bottom: 5px;
+	
+	background: rgba(0, 0, 0, 0.1);
+	
+	
+
+}
+
+.item_description{
+	
+	
+	width: 83%;
+	height: 70%;
+	
+	text-align: center;
+	
+	float:left;
+	
+}
+
+
+.item_pic{
+	
+	height: inherit;
+	width: 17%;
+	
+	background-color: #CC33CC;
+	
+	float: left;
+}
+
+
+#description_title_hover{
+	
+	font-style: italic;
+	color: #00cc33;
+	
+}
